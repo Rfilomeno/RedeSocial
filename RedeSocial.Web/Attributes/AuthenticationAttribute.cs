d12805be5b1ab29e3eac14d1bec3bbd45d0a2e8b
Rodrigo Filomeno <rodrigo.filomeno@al.infnet.edu.br>
2017-04-25 14:54:22 -0300

    finalizando roteiro etapa 6

---
+++ RedeSocial.Web/Attributes/AuthenticationAttribute.cs
@@ -0,0 +1,25 @@
+﻿using RedeSocial.Web.Helpers;
+using System.Web.Mvc;
+
+
+namespace RedeSocial.Web.Attributes
+{
+    public class AuthenticationAttribute : ActionFilterAttribute
+    {
+        private readonly TokenHelper _tokenHelper;
+
+        public AuthenticationAttribute()
+        {
+            _tokenHelper = new TokenHelper();
+        }
+
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            if (_tokenHelper.AccessToken == null)
+            {
+                filterContext.HttpContext.Response.RedirectToRoute("Login");
+            }
+        }
+
+    }
+}
--- RedeSocial.Web/Controllers/AccountController.cs
+++ RedeSocial.Web/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
-﻿using RedeSocial.Web.Models.Account;
+﻿using Newtonsoft.Json.Linq;
+using RedeSocial.Web.Helpers;
+using RedeSocial.Web.Models.Account;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,17 +15,20 @@ namespace RedeSocial.Web.Controllers
     public class AccountController : Controller
     {
         private HttpClient _client;
+        private TokenHelper _tokenHelper;
 
         public AccountController()
         {
             _client = new HttpClient();
-
+            
             _client.BaseAddress = new Uri("https://localhost:44346/");
             _client.DefaultRequestHeaders.Accept.Clear();
 
             var mediaType = new MediaTypeWithQualityHeaderValue("application/json");
 
             _client.DefaultRequestHeaders.Accept.Add(mediaType);
+
+            _tokenHelper = new TokenHelper();
         }
         // GET: Account
         public ActionResult Register()
@@ -60,5 +65,56 @@ namespace RedeSocial.Web.Controllers
             }
             base.Dispose(disposing);
         }
+
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        //protected override void Dispose(bool disposing)
+        //{
+        //    if (disposing && _client != null)
+        //    {
+        //        _client.Dispose();
+        //        _client = null;
+        //    }
+        //    base.Dispose(disposing);
+        //}
+
+        // POST: Account/Login
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var data = new Dictionary<string, string>()
+                {
+                    {"grant_type", "password" },
+                    {"username", model.Email },
+                    {"password", model.Password }
+                };
+                using(var requestContent = new FormUrlEncodedContent(data))
+                {
+                    var response = await _client.PostAsync("/Token", requestContent);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var responseContent = await response.Content.ReadAsStringAsync();
+                        var tokenData = JObject.Parse(responseContent);
+
+                        _tokenHelper.AccessToken = tokenData["access_token"];
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "");
+                    }
+                }
+            }
+
+            return View(model);
+        }
+
+        
     }   
 }
